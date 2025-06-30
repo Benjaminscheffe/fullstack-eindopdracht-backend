4cad0c6355a7c9c375b76f6bcca652be13b480d9
benjaminscheffe <benjaminscheffe@hotmail.com>
2025-06-30 10:42:39 +0200

    basis/start project

---
+++ src/main/java/nl/benjamin/muziekmarktplaats/MuziekmarktplaatsApplication.java
@@ -0,0 +1,13 @@
+package nl.benjamin.muziekmarktplaats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuziekmarktplaatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuziekmarktplaatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/benjamin/muziekmarktplaats/MuziekmarktplaatsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.benjamin.muziekmarktplaats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuziekmarktplaatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
